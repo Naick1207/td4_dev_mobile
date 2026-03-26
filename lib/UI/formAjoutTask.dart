b4d80efd9599836ff43e167e906dde0c08916701
Naick1207 <naick.cher@etu.univ-orleans.fr>
2026-03-26 09:29:43 +0100

    Avancé du projet

--- lib/UI/formAjoutTask.dart
+++ lib/UI/formAjoutTask.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:td2/model/task.dart';
 import 'package:td2/viewModel/taskViewModel.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart'
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class FormAjoutTask extends StatefulWidget {
     const FormAjoutTask({super.key});
@@ -14,6 +14,9 @@ class FormAjoutTask extends StatefulWidget {
 class _FormAjoutTaskState extends State<FormAjoutTask> {
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     final TextEditingController textTitleController = TextEditingController();
+    final TextEditingController textDescriptionController = TextEditingController();
+
+    int _currentDifficulty = 1;
 
     @override dispose() {
         textTitleController.dispose();
@@ -37,13 +40,29 @@ class _FormAjoutTaskState extends State<FormAjoutTask> {
                             return null;
                         },
                     ),
+                    TextFormField(
+                        controller: textDescriptionController,
+                        decoration: const InputDecoration(hintText: 'Description'),
+                        validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Veuillez insérer du texte';
+                            }
+                            return null;
+                        },
+                    ),
                     FormBuilderSlider(
+                        name: "Difficulté",
                         min: 1,
                         max: 5,
                         divisions: 4,
-                        value: 1,
-                        label; 'Difficulté',
-                        onChanged: (val) => setState(() => value = val.round()),
+                        initialValue: 1,
+                        onChanged: (val) {
+                          if (val != null) {
+                            setState(() {
+                              _currentDifficulty = val.round();
+                            });
+                          }
+                        },
                     ),
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 16.0),
@@ -57,7 +76,13 @@ class _FormAjoutTaskState extends State<FormAjoutTask> {
                                     ScaffoldMessenger.of(context).showSnackBar(
                                         const SnackBar(content: Text('Tâche créée')),
                                     );
-                                    context.read<TaskViewModel>().addTask(Task.newTaskTitle(textTitleController.text));
+                                    context.read<TaskViewModel>().addTask(
+                                        Task.newTaskTitle(textTitleController.text,
+                                        0,
+                                        _currentDifficulty,
+                                        textDescriptionController.text,
+                                        ),
+                                    );
                                     Navigator.pop(context);
                                 }
                             },
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import "package:flutter/foundation.dart";
+import "package:flutter/cupertino.dart";
 import 'package:flutter/material.dart';
 import "UI/myApp.dart";
+import "package:sqflite/sqflite.dart";
+import "package:sqflite_common_ffi_web/sqflite_ffi_web.dart";
+import "package:path/path.dart";
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'task_database.db'),
+    onCreate: (db, version) {
+      await db.execute(
+          'CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, tags TEXT, nbhours INTEGER, difficulty INTEGER)'
+      );
+      await db.execute(
+          'CREATE TABLE tag (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)'
+      );
+      await db.execute(
+          'CREATE TABLE task_tag (task_id INTEGER, tag_id INTEGER, FOREIGN KEY task_id REFERENCES task(id), FOREIGN KEY tag_id REFERENCES tag(id))'
+      );
+    }
+  );
   runApp(const MyApp());
 }
